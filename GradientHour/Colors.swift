ffee4623497a58564141f5242194f65859432dd2
saul51292 <saulhamadani@me.com>
2015-01-08 21:03:29 -0500

    First Commit

---
+++ GradientHour/Colors.swift
@@ -0,0 +1,189 @@
+//
+//  Colors.swift
+//  GradientHour
+//
+//  Created by Saul on 1/8/15.
+//  Copyright (c) 2015 Saul. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+class Colors {
+    //Zero
+    var zeroTopColor = UIColor(red: (0/255.0), green: (17/255.0), blue: (72/255.0), alpha: 1.0)
+    var zeroBottomColor = UIColor(red: (2/255.0), green: (19/255.0), blue: (34/255.0), alpha: 1.0)
+    //One
+    var oneTopColor = UIColor(red: (1/255.0), green: (36/255.0), blue: (88/255.0), alpha: 1.0)
+    //Two
+    var twoTopcolor = UIColor(red: (7/255.0), green: (59/255.0), blue: (115/255.0), alpha: 1.0)
+    //Three
+    var threeTopColor = UIColor(red: (13/255.0), green: (67/255.0), blue: (114/255.0), alpha: 1.0)
+    var threeBottomColor = UIColor(red: (3/255.0), green: (24/255.0), blue: (43/255.0), alpha: 1.0)
+    //Four
+    
+    var fourTopColor = UIColor(red: (27/255.0), green: (94/255.0), blue: (143/255.0), alpha: 1.0)
+    //Five
+    var fiveTopColor = UIColor(red: (33/255.0), green: (104/255.0), blue: (146/255.0), alpha: 1.0)
+    //Six
+    var sixTopColor = UIColor(red: (39/255.0), green: (116/255.0), blue: (161/255.0), alpha: 1.0)
+    var sixBottomColor = UIColor(red: (9/255.0), green: (46/255.0), blue: (74/255.0), alpha: 1.0)
+    //Seven
+    var sevenTopColor = UIColor(red: (64/255.0), green: (162/255.0), blue: (192/255.0), alpha: 1.0)
+    var sevenBottomColor = UIColor(red: (29/255.0), green: (84/255.0), blue: (113/255.0), alpha: 1.0)
+    //Eight
+    var eightTopColor = UIColor(red: (117/255.0), green: (212/255.0), blue: (204/255.0), alpha: 1.0)
+    var eightBottomColor = UIColor(red: (51/255.0), green: (135/255.0), blue: (167/255.0), alpha: 1.0)
+    //Nine
+    var nineTopColor = UIColor(red: (236/255.0), green: (237/255.0), blue: (189/255.0), alpha: 1.0)
+    var nineBottomColor = UIColor(red: (97/255.0), green: (208/255.0), blue: (207/255.0), alpha: 1.0)
+    //Ten
+    var tenTopColor = UIColor(red: (252/255.0), green: (225/255.0), blue: (86/255.0), alpha: 1.0)
+    var tenBottomColor = UIColor(red: (163/255.0), green: (222/255.0), blue: (198/255.0), alpha: 1.0)
+    //Eleven
+    var elevenTopColor = UIColor(red: (249/255.0), green: (194/255.0), blue: (82/255.0), alpha: 1.0)
+    var elevenBottomColor = UIColor(red: (232/255.0), green: (236/255.0), blue: (148/255.0), alpha: 1.0)
+    //Twelve
+    var twelveTopColor = UIColor(red: (245/255.0), green: (172/255.0), blue: (119/255.0), alpha: 1.0)
+    var twelveBottomColor = UIColor(red: (254/255.0), green: (228/255.0), blue: (103/255.0), alpha: 1.0)
+    //Thirteen
+    var thirteenTopColor = UIColor(red: (244/255.0), green: (166/255.0), blue: (90/255.0), alpha: 1.0)
+    //Fourteen
+    var fourteenTopColor = UIColor(red: (243/255.0), green: (158/255.0), blue: (88/255.0), alpha: 1.0)
+    //Fifteen
+    var fifteenTopColor = UIColor(red: (240/255.0), green: (132/255.0), blue: (72/255.0), alpha: 1.0)
+    var fifteenBottomColor = UIColor(red: (251/255.0), green: (208/255.0), blue: (98/255.0), alpha: 1.0)
+    //Sixteen
+    var sixteenTopColor = UIColor(red: (236/255.0), green: (95/255.0), blue: (112/255.0), alpha: 1.0)
+    var sixteenBottomColor = UIColor(red: (245/255.0), green: (168/255.0), blue: (86/255.0), alpha: 1.0)
+    //Seventeen
+    var seventeenTopColor = UIColor(red: (199/255.0), green: (78/255.0), blue: (136/255.0), alpha: 1.0)
+    var seventeenBottomColor = UIColor(red: (241/255.0), green: (139/255.0), blue: (110/255.0), alpha: 1.0)
+    //Eighteen
+    var eighteenTopColor = UIColor(red: (91/255.0), green: (44/255.0), blue: (131/255.0), alpha: 1.0)
+    var eighteenBottomColor = UIColor(red: (209/255.0), green: (98/255.0), blue: (139/255.0), alpha: 1.0)
+    //Nineteen
+    var nineteenTopColor = UIColor(red: (56/255.0), green: (34/255.0), blue: (121/255.0), alpha: 1.0)
+    var nineteenBottomColor = UIColor(red: (113/255.0), green: (54/255.0), blue: (132/255.0), alpha: 1.0)
+    //Twenty
+    var twentyTopColor = UIColor(red: (42/255.0), green: (29/255.0), blue: (106/255.0), alpha: 1.0)
+    var twentyBottomColor = UIColor(red: (69/255.0), green: (37/255.0), blue: (124/255.0), alpha: 1.0)
+    //Twenty-One
+    var twentyOneTopColor = UIColor(red: (25/255.0), green: (40/255.0), blue: (97/255.0), alpha: 1.0)
+    var twentyOneBottomColor = UIColor(red: (58/255.0), green: (36/255.0), blue: (119/255.0), alpha: 1.0)
+    //Twenty-Two
+    var twentyTwoTopColor = UIColor(red: (7/255.0), green: (14/255.0), blue: (62/255.0), alpha: 1.0)
+    var twentyTwoBottomColor = UIColor(red: (36/255.0), green: (49/255.0), blue: (114/255.0), alpha: 1.0)
+    //Twenty-Three
+    var twentyThreeTopColor = UIColor(red: (6/255.0), green: (14/255.0), blue: (62/255.0), alpha: 1.0)
+    var twentyThreeBottomColor = UIColor(red: (0/255.0), green: (35/255.0), blue: (88/255.0), alpha: 1.0)
+    
+    
+    
+    var toArray = [UIColor(red: (0/255.0), green: (17/255.0), blue: (72/255.0), alpha: 1.0).CGColor, UIColor(red: (2/255.0), green: (19/255.0), blue: (34/255.0), alpha: 1.0).CGColor]
+    
+    let gl: CAGradientLayer
+    
+    init() {
+        gl = CAGradientLayer()
+        gl.colors = toArray
+        gl.locations = [ 0.0, 1.0]
+    }
+    
+    
+    func getHourOfDay()->Int {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate: date)
+        let hour = components.hour
+        println("The hour is \(hour)")
+        
+        return hour
+    }
+    
+    func setArrays() -> [CGColor!]
+    {
+        
+        var oneArray = [oneTopColor.CGColor, zeroBottomColor.CGColor]
+        var twoArray = [twoTopcolor.CGColor, zeroBottomColor.CGColor]
+        var threeArray = [threeTopColor.CGColor, threeBottomColor.CGColor]
+        var fourArray = [fourTopColor.CGColor, threeBottomColor.CGColor]
+        var fiveArray = [fiveTopColor.CGColor, threeBottomColor.CGColor]
+        var sixArray = [sixTopColor.CGColor, sixBottomColor.CGColor]
+        var sevenArray = [sevenTopColor.CGColor, sevenBottomColor.CGColor]
+        var eightArray = [eightTopColor.CGColor, eightBottomColor.CGColor]
+        var nineArray = [nineTopColor.CGColor, nineBottomColor.CGColor]
+        var tenArray = [tenTopColor.CGColor, tenBottomColor.CGColor]
+        var elevenArray = [elevenTopColor.CGColor, elevenBottomColor.CGColor]
+        var twelveArray = [twelveTopColor.CGColor, twelveBottomColor.CGColor]
+        var thirteenArray = [thirteenTopColor.CGColor, twelveBottomColor.CGColor]
+        var fourteenArray = [fourteenTopColor.CGColor, twelveBottomColor.CGColor]
+        var fifteenArray = [fifteenTopColor.CGColor, fifteenBottomColor.CGColor]
+        var sixteenArray = [sixteenTopColor.CGColor, sixteenBottomColor.CGColor]
+        var seventeenArray = [seventeenTopColor.CGColor, seventeenBottomColor.CGColor]
+        var eighteenArray = [eighteenTopColor.CGColor, eighteenBottomColor.CGColor]
+        var nineteenArray = [nineteenTopColor.CGColor, nineteenBottomColor.CGColor]
+        var twentyArray = [twentyTopColor.CGColor, twentyBottomColor.CGColor]
+        var twentyOneArray = [twentyOneTopColor.CGColor, twentyOneBottomColor.CGColor]
+        var twentyTwoArray = [twentyTwoTopColor.CGColor, twentyTwoBottomColor.CGColor]
+        var twentyThreeArray = [twentyThreeTopColor.CGColor, twentyThreeBottomColor.CGColor]
+        
+        
+        
+        var hour = getHourOfDay()
+        
+        switch hour  {
+        case 1:
+            toArray = oneArray
+        case 2:
+            toArray = twoArray
+        case 3:
+            toArray = threeArray
+        case 4:
+            toArray = fourArray
+        case 5:
+            toArray = fiveArray
+        case 6:
+            toArray = sixArray
+        case 7:
+            toArray = sevenArray
+        case 8:
+            toArray = eightArray
+        case 9:
+            toArray = nineArray
+        case 10:
+            toArray = tenArray
+        case 11:
+            toArray = elevenArray
+        case 12:
+            toArray = twelveArray
+        case 13:
+            toArray = thirteenArray
+        case 14:
+            toArray = fourteenArray
+        case 15:
+            toArray = fifteenArray
+        case 16:
+            toArray = sixteenArray
+        case 17:
+            toArray = seventeenArray
+        case 18:
+            toArray = eighteenArray
+        case 19:
+            toArray = nineteenArray
+        case 20:
+            toArray = twentyArray
+        case 21:
+            toArray = twentyOneArray
+        case 22:
+            toArray = twentyTwoArray
+        case 23:
+            toArray = twentyThreeArray
+            
+        default:
+            return toArray
+        }
+        return toArray
+    }
+}
--- GradientHour/ViewController.swift
+++ GradientHour/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
-
+    
+    let colors = Colors()
+    var backgroundLayer = Colors().gl
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, atIndex: 0)
+        animateLayer()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
+    func animateLayer() {
+        
+        var array = backgroundLayer.colors
+        
+        colors.setArrays()
+        
+        backgroundLayer.colors = colors.toArray
+        
+        var animation = CABasicAnimation (keyPath: "colors")
+        
+        animation.fromValue = array
+        animation.toValue = colors.toArray
+        animation.duration = 3.00
+        animation.removedOnCompletion = true
+        animation.fillMode = kCAFillModeForwards
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.delegate = self
+        
+        backgroundLayer.addAnimation(animation, forKey: "animateGradient")
+        
+    }
+    
 }
-
